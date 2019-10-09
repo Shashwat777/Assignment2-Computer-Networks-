0631c3f8328f5d52c57684ad13c4d380b1d232aa
shashwat <shashwatjain2014@gmail.com>
2019-10-10 02:37:27 +0530

    commit

---
+++ client.py
@@ -0,0 +1,41 @@
+import socket
+import sys
+import time
+
+# Create a TCP/IP et
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the et to the port where the server is listening
+server_address = ('localhost', 10000)
+print >>sys.stderr, 'connecting to %s port %s' % server_address
+sock.connect(server_address)
+try:
+
+    # Send data
+    message = 'a particular port at an IP, while other socket reaches out to the other to form a connection. Server forms the listener socket while client reaches out to the serverSocket programming is a way of connecting two nodes on a network to communicate with each other. One socket(node) listens on a particular port at an IP, while other socket reaches out to the other to form a connection. Server forms the listener socket while client reaches out to the serverSocket=ient reaches out to the serverSocker. , while other socket reaches out to the other to form a connection. Server forms the listener socket while client reaches out to the serverSocket programming is a way of connecting two nodes on a network to communicate with each other. One on a particular port at an IP, while other socket reaches out to the other to form a connection. Server forms the listener socket while client reaches out to the serveruu'
+    print ("sending data of size: "+str(sys.getsizeof(message)))
+    sent_time = int(round(time.time() * 1000))
+    sock.sendall(message)
+
+    # Look for the response
+    amount_received = 0
+    amount_expected = len(message)
+    data=""
+
+    while True:
+
+        data = sock.recv(1)
+
+
+        ackRcvd_time = int(round(time.time() * 1000))
+        if(data=="A"):
+            break
+
+    print ("Ack rcvd:"+data)
+    print (str(ackRcvd_time-sent_time)+"ms Round trip delay")
+
+
+
+finally:
+    print >>sys.stderr, 'closing socket'
+    sock.close()
---
+++ server.py
@@ -0,0 +1,31 @@
+import socket
+import sys
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Bind the socket to the port
+server_address = ('localhost', 10000)
+print >>sys.stderr, 'starting up on %s port %s' % server_address
+sock.bind(server_address)
+# Listen for incoming connections
+sock.listen(1)
+
+while True:
+    # Wait for a connection
+    print >>sys.stderr, 'waiting for a connection'
+    connection, client_address = sock.accept()
+    try:
+        print >>sys.stderr, 'connection from', client_address
+
+        # Receive the data in small chunks and retransmit it
+        data=""
+
+        data = data+connection.recv(2000)
+        print >>sys.stderr, 'received "%s"' % data
+        connection.sendall("A")
+        print >>sys.stderr, 'sending Ack to the client'
+        connection.sendall("A")
+
+    finally:
+        # Clean up the connection
+        connection.close()
